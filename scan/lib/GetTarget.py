018cfaf59444f5aaae037bc7c63681f96bde9870
Joao Paulo <joaopaulo.a.p@gmail.com>
2018-12-08 10:20:20 -0200

    Update

--- scan/lib/GetTarget.py
+++ scan/lib/GetTarget.py
@@ -1,36 +1,37 @@
-'''
+"""
 Author: João Paulo Andrade
 Email: joaopauloap@gmail.com
 Script for get range allocated ipv4/ipv6 from lacnic
-'''
+"""
 
 from ftplib import FTP
 
-class GetTarget:
 
-    def __init__(self): #Download of data
+class GetTarget:
+    # Download of data
+    def __init__(self):
         self._ftp = FTP('ftp.lacnic.net')
         self._ftp.login()
         self._ftp.cwd('/pub/stats/lacnic/')
         self._ftp.retrbinary('RETR delegated-lacnic-latest', open('lacnic-latest', 'wb').write)
 
-    def gen_target(self, file):
-
-        def get_mask(amount):#Tranform amount ips in mask CIDR
+    @staticmethod
+    def gen_target(file):
+        # Tranform amount ips in mask CIDR
+        def get_mask(amount):
             mask = 0
             while amount != 1:
                 amount = amount / 2
                 mask += 1
             return 32 - mask
 
-        self.file = file
-        f = open(self.file,'r')
-        l = f.readlines()
-        l = [x.strip().split('|') for x in l]
-        for x in l:
+        f = open(file, 'r')
+        l_raw = f.readlines()
+        l_format = [x.strip().split('|') for x in l_raw]
+        for x in l_format:
             if x.__len__() == 7:
                 if (x[2] == 'ipv4') and (x[6] == 'allocated'):
-                    z = open('ipv4_lacnic','a+')
+                    z = open('ipv4_lacnic', 'a+')
                     z.writelines('{}/{}\n'.format(x[3], get_mask(int(x[4]))))
                     z.close()
         f.close()
