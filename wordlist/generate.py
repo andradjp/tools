4d12ba22830b31b655849d08ebfd63fe363c71e7
Joao Paulo Andrade <joaopaulo.a.p@gmail.com>
2018-12-17 08:10:05 -0200

    ``

--- wordlist/generate.py
+++ wordlist/generate.py
@@ -1,7 +1,14 @@
 from itertools import permutations
 
-s = 'DIGOS0123456789'
-data = ''
-result = permutations(s, 8)
-for x in result:
-    print(data.join(x))
+def permutation():
+
+    s = 'DIGOS0123456789'
+    data = ''
+    result = permutations(s, 8)
+    for x in result:
+        print(data.join(x))
+
+def default_string():
+    for x in range(10000000000):
+        print(format('{:2}', x))
+default_string()
