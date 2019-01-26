77e78e78e682431bb986e151d65860358a3457bd
Joao Paulo <joaopaulo.a.p@gmail.com>
2019-01-26 20:57:08 -0200

    Deauth attack

--- wordlist/generate.py
+++ wordlist/generate.py
@@ -1,16 +1,18 @@
-from itertools import permutations
+from itertools import permutations, combinations_with_replacement
+from string import ascii_lowercase, digits
 
 def permutation():
-
-    s = '0123'
+    s = ascii_lowercase+digits
     data = ''
-    result = permutations(s, 4)
+    # result = permutations(s, 8) #Nao repete os caracteres
+    result = combinations_with_replacement(s, 8)
     for x in result:
         print(data.join(x))
 
 def default_string():
-    for x in range(1000000000):
+    for x in range(10**8):
         print(format(x, '08'))
+
 
 # default_string()
 permutation()
