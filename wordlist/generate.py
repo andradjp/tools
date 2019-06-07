067f039cc14ab204b717ce54c937d6ce24a7de92
Joao Paulo <joaopaulo.a.p@gmail.com>
2019-06-07 19:48:49 -0300

    Deauth attack

--- wordlist/generate.py
+++ wordlist/generate.py
@@ -1,21 +1,31 @@
-from itertools import permutations, combinations_with_replacement, starmap
+from itertools import permutations, combinations_with_replacement, starmap, combinations
 from string import ascii_lowercase, digits
 
-keys = 9
+keys = 8
 
 def permutation():
-    s = ascii_lowercase
-    data = ''
-    # result = permutations(s, keys) #Nao repete os caracteres
-    # result = combinations_with_replacement(s, 4)
-    result = combinations_with_replacement(s, 2)
-    for x in result:
-        print(data.join(x))
+    x = 'abcdef' + digits
+    for x1 in x:
+        for x2 in x:
+            for x3 in x:
+                for x4 in x:
+                    for x5 in x:
+                        for x6 in x:
+                            for x7 in x:
+                                for x8 in x:
+                                    print('{}{}{}{}{}{}{}{}'.format(x1,x2,x3,x4,x5,x6,x7,x8))
+
+
+def concatenado():
+    s = 'Geovana'
+    for x in range(1000):
+        print(s+str(x))
+
 
 def default_string():
     for x in range(10**keys):
         print(format(x, '0{}'.format(keys)))
 
-
-default_string()
-#permutation()
+# default_string()
+permutation()
+# concatenado()
