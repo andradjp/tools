9e53c03accc6532f7aa8e646479f3074fdb23d37
Joao Paulo Andrade <contact@jpandrade.info>
2020-02-14 13:39:22 +0000

    update chromedriver_macos

--- MegaSena/main.py
+++ MegaSena/main.py
@@ -7,10 +7,11 @@ Data: 11.07.2019
 from MegaSena import last_games
 from MegaSena import megasena
 
-qt_dezenas = 7 #Quantidade de dezenas por jogo
-qt_jogos = 17 #Quantidade de jogos
+qt_dezenas = 8 #Quantidade de dezenas por jogo
+qt_jogos = 10 #Quantidade de jogos
 
 if __name__ == '__main__':
     last_games.start()
     for x in range(qt_jogos):
         megasena.gera_jogo_mega_sena(qt_dezenas)
+    exit()
